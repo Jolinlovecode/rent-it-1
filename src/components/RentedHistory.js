cfbebf8be8cf80830c7cfa2d4f4a9ca772646241
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-28 15:16:05 -0600

    render rented history

--- src/components/RentedHistory.js
+++ src/components/RentedHistory.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useStateValue } from "../providers/StateProvider";
+import OnRentingItem from './OnRentingItem';
 
 /**
  * Act as receipt page
  * Keep rented history of the item
  */
 export default function RentedHistory() {
-  const rentedItems = [];
+
+  const [{rentedHistory}] = useStateValue();
+  const currentlyRentingHistory = rentedHistory.map((item) => (
+    <OnRentingItem
+      key={item.id}
+      {...item}
+    />
+  ));
+
   return (
     <div className='section'>
       <div className='container'>
         <div className='title is-4'>Rented History</div>
         <div className="column columns is-multiline">
-          {rentedItems.length ? rentedItems : 
+          {rentedHistory.length ? currentlyRentingHistory : 
+          
             ( <div className="column">
                 <span className="title has-text-grey-light">No items found!</span>
               </div> )
