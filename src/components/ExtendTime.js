818cc4a89ca3f5b56082b40cfc428277970aee56
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-04-02 14:35:12 -0600

    delete unused code

--- src/components/ExtendTime.js
+++ src/components/ExtendTime.js
@@ -140,7 +140,6 @@ export default function ExtendTime(props) {
                   prefix={"USD $"}
           />)}
               </div>
-              <div className='field is-pulled-right'>payment api</div>
             </div>
           </div>
         ): ""}
